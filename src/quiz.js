250a0e33c9ebfbd4d9a7d79a09fe1f98da1be274
Julia Solias <juliasohu@gmail.com>
2025-01-20 16:43:21 +0100

    Finalised Day 1

--- src/quiz.js
+++ src/quiz.js
@@ -1,15 +1,37 @@
 class Quiz {
-    // YOUR CODE HERE:
-    //
-    // 1. constructor (questions, timeLimit, timeRemaining)
+    constructor (questions, timeLimit, timeRemaining){
+      this.questions = questions;
+      this.timeLimit = timeLimit;
+      this.timeRemaining = timeRemaining;
+      this.correctAnswers = 0;
+      this.currentQuestionIndex = 0;
+  }
 
-    // 2. getQuestion()
-    
-    // 3. moveToNextQuestion()
+ getQuestion(){
+  return this.questions[this.currentQuestionIndex];
+ }
+  
+ moveToNextQuestion(){
+  this.currentQuestionIndex++;
+ }
 
-    // 4. shuffleQuestions()
+ shuffleQuestions(){
+  for (let i = this.questions.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1));
+      [this.questions[i], this.questions[j]] = [this.questions[j], this.questions[i]];
+    }
+    return this.questions;
+ }
 
-    // 5. checkAnswer(answer)
+  checkAnswer(answer){
+      this.correctAnswers++
+  }
 
-    // 6. hasEnded()
+  hasEnded(){
+      if (this.currentQuestionIndex < this.questions.length) {
+          return false
+      } else if (this.currentQuestionIndex === this.questions.length) {
+          return true
+      }
+  }
 }
